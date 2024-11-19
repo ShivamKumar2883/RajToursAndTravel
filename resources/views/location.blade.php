c9046cb74250170f0cf1ef42e08f9d10be916d8a
Shivam Kumar <shivamkumar29833@gmail.com>
2024-11-19 19:28:22 +0530

    Initial commit for Raj Tours and Travel

---
+++ resources/views/location.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.myapp')
+@section('content')
+    <div class="mx-auto max-w-screen-xl ">
+        <div class="flex md:flex-row flex-col justify-around  items-center px-6 pt-6">
+            <div class="md:m-12 p-6 md:w-1/2">
+                <img loading="lazy" src="/images/shop1.png" alt="shop image">
+            </div>
+            <div class=" relative md:m-12 m-6 md:w-1/2 md:p-6">
+
+                <p>Welcome to Raj Tour & Travels, the premier car rental shop in Haridwar, conveniently situated in the city’s heart. Our prime location offers both locals and travelers easy access to quality rental vehicles, ensuring a smooth start to your journey, whether it's for business, leisure, or pilgrimage. Finding us is simple, and our central spot makes us the ideal choice for exploring Haridwar and beyond.</p>
+                <br>
+                <p>Our shop is strategically positioned near Haridwar’s main transportation hubs, such as the railway station and major bus terminals. This makes it effortless for you to pick up and drop off your rental car with minimal hassle. Our friendly staff are here to greet you upon arrival, making sure you experience an efficient and seamless rental process from the moment you walk in.
+
+</p>
+            </div>
+
+        </div>
+        <div class="flex md:flex-row flex-col justify-around  items-center px-6 pt-6">
+
+            <div class="md:m-12 p-6 md:w-1/2 md:order-last ">
+                <img loading="lazy" src="/images/shop_2.png" alt="shop image">
+            </div>
+
+            <div class=" relative md:m-12 m-6 md:w-1/2 md:p-6">
+                <p>Situated in a vibrant neighborhood, Raj Tour & Travels is surrounded by a variety of local amenities. You’ll find plenty of restaurants, cafes, and shopping options nearby, making it easy to grab a meal, pick up essentials, or enjoy some downtime before or after your rental. This convenient location adds to the ease of starting and ending your journey with us.</p>
+                <br>
+                <p>We also offer ample parking space at our location, so you can park your own vehicle comfortably while you drive off in one of ours. At Raj Tour & Travels, we prioritize your convenience and comfort, providing a smooth and enjoyable rental experience every time.</p>
+            </div>
+
+
+        </div>
+        <div style="width: 100%; background-color: black; padding: 20px; border-radius: 10px; margin-bottom: 20px; box-sizing: border-box;">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3457.2823791662363!2d78.06675779999999!3d29.942555299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390949ad35546e71%3A0xcb2e86c9aa3b39ca!2sRaj%20Tour%20and%20Travels!5e0!3m2!1sen!2sin!4v1731432744160!5m2!1sen!2sin" width="100%" height="450" style="border:0; border-radius: 8px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+
+
+    </div>
+@endsection
